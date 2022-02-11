4ca2f04bd8c7d77221db63378473f44954363ee5
Stefano Bognanni <50678436+stefanellobognannello@users.noreply.github.com>
2022-02-11 10:16:08 +0100

    Update scanNetwork.py

---
+++ Phonejack/scanNetwork.py
@@ -0,0 +1,14 @@
+import nmap 
+
+
+def scanNetwork(network):
+    hosts = []
+    nm = nmap.PortScanner()
+    out = nm.scan(hosts=network, arguments='-sP')
+    for k, v in a['scan'].iteritems():
+        if str(v['status']['state']) == 'up':
+            try:
+                returnlist.append([str(v['addresses']['ipv4']), str(v['addresses']['mac'])])
+            except:
+                pass
+    print hosts 
